b7eefc2e69920a871be2912ca300be243be1aa51
Nikita Aplin <aplinxy9plin@icloud.com>
2017-12-07 20:31:00 +0700

    Commit

---
+++ RB.ru/RB.ru/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RB.ru
+//
+//  Created by Никита Аплин on 07.12.2017.
+//  Copyright © 2017 Никита Аплин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.loadRequest(URLRequest(url: URL(string: "http://hardwirestudios.com")!))
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
